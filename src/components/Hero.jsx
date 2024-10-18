2b228130ea5b908ef9d0d8c804c7ef24cd4c8a57
M3 <may.myat.mon@flymya.co>
2024-10-18 14:14:35 +0630

    animated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,70 @@
-import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useEffect, useState } from "react";
+import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrc = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrc);
+    //cleanup event listener
+    return () => {
+      window.removeEventListener("resize", handleVideoSrc);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 2,
+      ease: "power1.inOut",
+    });
+    gsap.to("#cta", {
+      opacity: 1,
+      y: -50,
+      delay: 2,
+    });
+  }, []);
+
   return (
-    <div className="w-full bg-black nav-height relative">
-      <h1 className="h-5/6 w-full flex-center flex-1">
-        <p className="hero-title">iphone 15 Pro</p>
-      </h1>
-    </div>
+    <section className="w-full bg-black nav-height relative">
+      <div className="h-5/6 flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iphone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
+      </div>
+    </section>
   );
 };
 
